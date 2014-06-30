ea1ebd079edb0ffcadcf79d17efe0eda00e8ebc7
VISTALL <vistall.valeriy@gmail.com>
2014-06-30 20:08:12 +0400

    override + format

--- src/main/java/org/jetbrains/idea/maven/wizards/MavenProjectImportProvider.java
+++ src/main/java/org/jetbrains/idea/maven/wizards/MavenProjectImportProvider.java
@@ -20,6 +20,11 @@
  */
 package org.jetbrains.idea.maven.wizards;
 
+import java.io.File;
+
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.idea.maven.project.MavenProject;
+import org.jetbrains.idea.maven.project.MavenProjectsManager;
 import com.intellij.ide.util.projectWizard.ModuleWizardStep;
 import com.intellij.ide.util.projectWizard.ProjectWizardStepFactory;
 import com.intellij.ide.util.projectWizard.WizardContext;
@@ -29,66 +34,85 @@ import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.projectImport.ProjectImportProvider;
 import com.intellij.projectImport.SelectImportedProjectsStep;
-import org.jetbrains.annotations.Nullable;
-import org.jetbrains.idea.maven.project.MavenProject;
-import org.jetbrains.idea.maven.project.MavenProjectsManager;
 
-import java.io.File;
+public class MavenProjectImportProvider extends ProjectImportProvider
+{
+	public MavenProjectImportProvider(final MavenProjectBuilder builder)
+	{
+		super(builder);
+	}
 
-public class MavenProjectImportProvider extends ProjectImportProvider {
-  public MavenProjectImportProvider(final MavenProjectBuilder builder) {
-    super(builder);
-  }
+	@Override
+	public ModuleWizardStep[] createSteps(final WizardContext wizardContext)
+	{
+		final ProjectWizardStepFactory stepFactory = ProjectWizardStepFactory.getInstance();
+		return new ModuleWizardStep[]{
+				new MavenProjectImportStep(wizardContext),
+				new SelectProfilesStep(wizardContext),
+				new SelectImportedProjectsStep<MavenProject>(wizardContext)
+				{
+					@Override
+					protected String getElementText(final MavenProject project)
+					{
+						final StringBuilder stringBuilder = new StringBuilder();
+						stringBuilder.append(project.getMavenId());
+						VirtualFile root = ((MavenProjectBuilder) getBuilder()).getRootDirectory();
+						if(root != null)
+						{
+							final String relPath = VfsUtilCore.getRelativePath(project.getDirectoryFile(), root, File.separatorChar);
+							if(StringUtil.isNotEmpty(relPath))
+							{
+								stringBuilder.append(" [").append(relPath).append("]");
+							}
+						}
 
-  public ModuleWizardStep[] createSteps(final WizardContext wizardContext) {
-    final ProjectWizardStepFactory stepFactory = ProjectWizardStepFactory.getInstance();
-    return new ModuleWizardStep[]{new MavenProjectImportStep(wizardContext), new SelectProfilesStep(wizardContext),
-      new SelectImportedProjectsStep<MavenProject>(wizardContext) {
-        protected String getElementText(final MavenProject project) {
-          final StringBuilder stringBuilder = new StringBuilder();
-          stringBuilder.append(project.getMavenId());
-          VirtualFile root = ((MavenProjectBuilder)getBuilder()).getRootDirectory();
-          if (root != null) {
-            final String relPath = VfsUtilCore.getRelativePath(project.getDirectoryFile(), root, File.separatorChar);
-            if (StringUtil.isNotEmpty(relPath)) {
-              stringBuilder.append(" [").append(relPath).append("]");
-            }
-          }
+						if(!isElementEnabled(project))
+						{
+							stringBuilder.append(" (project is ignored. See Settings -> Maven -> Ignored Files)");
+						}
 
-          if (!isElementEnabled(project)) {
-            stringBuilder.append(" (project is ignored. See Settings -> Maven -> Ignored Files)");
-          }
+						return stringBuilder.toString();
+					}
 
-          return stringBuilder.toString();
-        }
+					@Override
+					protected boolean isElementEnabled(MavenProject mavenProject)
+					{
+						Project project = wizardContext.getProject();
+						if(project == null)
+						{
+							return true;
+						}
 
-        @Override
-        protected boolean isElementEnabled(MavenProject mavenProject) {
-          Project project = wizardContext.getProject();
-          if (project == null) return true;
+						return !MavenProjectsManager.getInstance(project).isIgnored(mavenProject);
+					}
 
-          return !MavenProjectsManager.getInstance(project).isIgnored(mavenProject);
-        }
+					@Override
+					public void updateDataModel()
+					{
+						super.updateDataModel();
+						getWizardContext().setProjectName(((MavenProjectBuilder) getBuilder()).getSuggestedProjectName());
+					}
 
-        public void updateDataModel() {
-          super.updateDataModel();
-          getWizardContext().setProjectName(((MavenProjectBuilder)getBuilder()).getSuggestedProjectName());
-        }
+					@Override
+					public String getHelpId()
+					{
+						return "reference.dialogs.new.project.import.maven.page3";
+					}
+				},
+				stepFactory.createNameAndLocationStep(wizardContext)
+		};
+	}
 
-        public String getHelpId() {
-          return "reference.dialogs.new.project.import.maven.page3";
-        }
-      }, stepFactory.createNameAndLocationStep(wizardContext)};
-  }
+	@Override
+	protected boolean canImportFromFile(VirtualFile file)
+	{
+		return "pom.xml".equals(file.getName());
+	}
 
-  @Override
-  protected boolean canImportFromFile(VirtualFile file) {
-    return "pom.xml".equals(file.getName());
-  }
-
-  @Nullable
-  @Override
-  public String getFileSample() {
-    return "<b>Maven</b> project file (pom.xml)";
-  }
+	@Nullable
+	@Override
+	public String getFileSample()
+	{
+		return "<b>Maven</b> project file (pom.xml)";
+	}
 }
