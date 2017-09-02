32b44b20b95d1c63b2eb61d8bc166dd803c6e4b6
VISTALL <vistall.valeriy@gmail.com>
2017-09-02 04:18:18 +0300

    drop duplicate ui

--- src/main/java/org/jetbrains/idea/maven/utils/MavenSettings.java
+++ src/main/java/org/jetbrains/idea/maven/utils/MavenSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2009 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,106 +15,147 @@
  */
 package org.jetbrains.idea.maven.utils;
 
-import com.intellij.openapi.options.Configurable;
-import com.intellij.openapi.options.ConfigurationException;
-import com.intellij.openapi.options.SearchableConfigurable;
-import com.intellij.openapi.project.Project;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JComponent;
+
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.idea.maven.execution.MavenRunner;
 import org.jetbrains.idea.maven.execution.MavenRunnerConfigurable;
 import org.jetbrains.idea.maven.execution.MavenRunnerSettings;
 import org.jetbrains.idea.maven.indices.MavenRepositoriesConfigurable;
-import org.jetbrains.idea.maven.project.*;
+import org.jetbrains.idea.maven.project.MavenGeneralConfigurable;
+import org.jetbrains.idea.maven.project.MavenGeneralSettings;
+import org.jetbrains.idea.maven.project.MavenIgnoredFilesConfigurable;
+import org.jetbrains.idea.maven.project.MavenImportingConfigurable;
+import org.jetbrains.idea.maven.project.MavenProjectsManager;
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.options.SearchableConfigurable;
+import com.intellij.openapi.project.Project;
+import consulo.annotations.RequiredDispatchThread;
 
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
+public class MavenSettings implements SearchableConfigurable.Parent
+{
+	public static final String DISPLAY_NAME = "Maven";
 
-public class MavenSettings implements SearchableConfigurable.Parent {
-  public static final String DISPLAY_NAME = "Maven";
+	private final Project myProject;
+	private final Configurable myConfigurable;
+	private final List<Configurable> myChildren;
 
-  private final Project myProject;
-  private final Configurable myConfigurable;
-  private final List<Configurable> myChildren;
+	public MavenSettings(@NotNull Project project)
+	{
+		myProject = project;
 
-  public MavenSettings(Project project) {
-    myProject = project;
+		myConfigurable = new MavenGeneralConfigurable()
+		{
+			@Override
+			protected MavenGeneralSettings getState()
+			{
+				return MavenProjectsManager.getInstance(myProject).getGeneralSettings();
+			}
+		};
 
-    myConfigurable = new MavenGeneralConfigurable() {
-      protected MavenGeneralSettings getState() {
-        return MavenProjectsManager.getInstance(myProject).getGeneralSettings();
-      }
-    };
+		myChildren = new ArrayList<>();
+		myChildren.add(new MavenImportingConfigurable(myProject));
+		myChildren.add(new MavenIgnoredFilesConfigurable(myProject));
 
-    myChildren = new ArrayList<Configurable>();
-    myChildren.add(new MavenImportingConfigurable(myProject));
-    myChildren.add(new MavenIgnoredFilesConfigurable(myProject));
+		myChildren.add(new MyMavenRunnerConfigurable(project));
 
-    myChildren.add(new MyMavenRunnerConfigurable(project));
+		//myChildren.add(new MavenTestRunningConfigurable(project));
 
-    if (!myProject.isDefault()) {
-      myChildren.add(new MavenRepositoriesConfigurable(myProject));
-    }
-  }
+		if(!myProject.isDefault())
+		{
+			myChildren.add(new MavenRepositoriesConfigurable(myProject));
+		}
+	}
 
-  public boolean hasOwnContent() {
-    return true;
-  }
+	@Override
+	public boolean hasOwnContent()
+	{
+		return true;
+	}
 
-  public boolean isVisible() {
-    return true;
-  }
+	@Override
+	public boolean isVisible()
+	{
+		return true;
+	}
 
-  public Runnable enableSearch(String option) {
-    return null;
-  }
+	@RequiredDispatchThread
+	@Override
+	public JComponent createComponent()
+	{
+		return myConfigurable.createComponent();
+	}
 
-  public JComponent createComponent() {
-    return myConfigurable.createComponent();
-  }
+	@RequiredDispatchThread
+	@Override
+	public boolean isModified()
+	{
+		return myConfigurable.isModified();
+	}
 
-  public boolean isModified() {
-    return myConfigurable.isModified();
-  }
+	@RequiredDispatchThread
+	@Override
+	public void apply() throws ConfigurationException
+	{
+		myConfigurable.apply();
+	}
 
-  public void apply() throws ConfigurationException {
-    myConfigurable.apply();
-  }
+	@RequiredDispatchThread
+	@Override
+	public void reset()
+	{
+		myConfigurable.reset();
+	}
 
-  public void reset() {
-    myConfigurable.reset();
-  }
+	@RequiredDispatchThread
+	@Override
+	public void disposeUIResources()
+	{
+		myConfigurable.disposeUIResources();
+	}
 
-  public void disposeUIResources() {
-    myConfigurable.disposeUIResources();
-  }
+	@Override
+	public Configurable[] getConfigurables()
+	{
+		return myChildren.toArray(new Configurable[myChildren.size()]);
+	}
 
-  public Configurable[] getConfigurables() {
-    return myChildren.toArray(new Configurable[myChildren.size()]);
-  }
+	@Override
+	@NotNull
+	public String getId()
+	{
+		return MavenSettings.class.getSimpleName();
+	}
 
-  @NotNull
-  public String getId() {
-    return MavenSettings.class.getSimpleName();
-  }
+	@Override
+	@Nls
+	public String getDisplayName()
+	{
+		return DISPLAY_NAME;
+	}
 
-  @Nls
-  public String getDisplayName() {
-    return DISPLAY_NAME;
-  }
+	@Override
+	public String getHelpTopic()
+	{
+		return myConfigurable.getHelpTopic();
+	}
 
-  public String getHelpTopic() {
-    return myConfigurable.getHelpTopic();
-  }
+	public static class MyMavenRunnerConfigurable extends MavenRunnerConfigurable
+	{
+		public MyMavenRunnerConfigurable(Project project)
+		{
+			super(project, false);
+		}
 
-  public static class MyMavenRunnerConfigurable extends MavenRunnerConfigurable {
-    public MyMavenRunnerConfigurable(Project project) {
-      super(project, false);
-    }
-
-    protected MavenRunnerSettings getState() {
-      return MavenRunner.getInstance(myProject).getState();
-    }
-  }
+		@Override
+		protected MavenRunnerSettings getState()
+		{
+			return MavenRunner.getInstance(myProject).getState();
+		}
+	}
 }
