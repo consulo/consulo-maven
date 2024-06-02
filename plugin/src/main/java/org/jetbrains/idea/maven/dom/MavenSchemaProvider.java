4de4b29761fd6b855f5a881281ab6328c3c8ff5f
Valery Semenchuk <vistall.valeriy@gmail.com>
2024-06-02 15:20:46 +0400

    fix errors with wrong schemes

--- plugin/src/main/java/org/jetbrains/idea/maven/dom/MavenSchemaProvider.java
+++ plugin/src/main/java/org/jetbrains/idea/maven/dom/MavenSchemaProvider.java
@@ -22,8 +22,10 @@ import consulo.xml.javaee.ExternalResourceManager;
 import consulo.xml.javaee.ExternalResourceManagerEx;
 import consulo.xml.javaee.ResourceRegistrar;
 import consulo.xml.javaee.StandardResourceProvider;
+import jakarta.annotation.Nonnull;
 
-import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
 
 @ExtensionImpl
 public class MavenSchemaProvider implements StandardResourceProvider
@@ -31,14 +33,34 @@ public class MavenSchemaProvider implements StandardResourceProvider
 	public static final String MAVEN_PROJECT_SCHEMA_URL = "http://maven.apache.org/xsd/maven-4.0.0.xsd";
 	public static final String MAVEN_PROFILES_SCHEMA_URL = "http://maven.apache.org/xsd/profiles-1.0.0.xsd";
 	public static final String MAVEN_SETTINGS_SCHEMA_URL = "http://maven.apache.org/xsd/settings-1.0.0.xsd";
+	public static final String MAVEN_SETTINGS_SCHEMA_URL_1_1 = "http://maven.apache.org/xsd/settings-1.1.0.xsd";
+	public static final String MAVEN_SETTINGS_SCHEMA_URL_1_2 = "http://maven.apache.org/xsd/settings-1.2.0.xsd";
 
+	public static final List<String> MAVEN_SETTINGS_SCHEMAS = List.of(MAVEN_SETTINGS_SCHEMA_URL,
+			MAVEN_SETTINGS_SCHEMA_URL_1_1,
+			MAVEN_SETTINGS_SCHEMA_URL_1_2);
+
+	@Override
 	public void registerResources(ResourceRegistrar registrar)
 	{
-		registrar.addStdResource("http://maven.apache.org/maven-v4_0_0.xsd", "/schemas/maven-4.0.0.xsd", getClass());
-		registrar.addStdResource(MAVEN_PROJECT_SCHEMA_URL, "/schemas/maven-4.0.0.xsd", getClass());
+		Map.of(
+				MAVEN_PROJECT_SCHEMA_URL, "/schemas/maven-4.0.0.xsd",
+				"http://maven.apache.org/maven-v4_0_0.xsd", "/schemas/maven-4.0.0.xsd",
+				MAVEN_PROFILES_SCHEMA_URL, "/schemas/profiles-1.0.0.xsd",
+				MAVEN_SETTINGS_SCHEMA_URL, "/schemas/settings-1.0.0.xsd",
+				MAVEN_SETTINGS_SCHEMA_URL_1_1, "/schemas/settings-1.1.0.xsd",
+				MAVEN_SETTINGS_SCHEMA_URL_1_2, "/schemas/settings-1.2.0.xsd"
+		).forEach((schemaUrl, schemaPath) -> addStdResource(registrar, schemaUrl, schemaPath));
+	}
 
-		registrar.addStdResource(MAVEN_PROFILES_SCHEMA_URL, "/schemas/profiles-1.0.0.xsd", getClass());
-		registrar.addStdResource(MAVEN_SETTINGS_SCHEMA_URL, "/schemas/settings-1.0.0.xsd", getClass());
+	private void addStdResource(ResourceRegistrar registrar, String schemaUrl, String schemaPath)
+	{
+		registrar.addStdResource(schemaUrl, schemaPath);
+		if(schemaUrl.startsWith("http://"))
+		{
+			String schemaUrlHttps = schemaUrl.replace("http://", "https://");
+			registrar.addStdResource(schemaUrlHttps, schemaPath);
+		}
 	}
 
 	@Nonnull
@@ -53,4 +75,3 @@ public class MavenSchemaProvider implements StandardResourceProvider
 		return result;
 	}
 }
-
