632dd9a3fc174d68c2170dfeeb0fb3a3a1834a94
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-04-15 20:46:58 +0400

    add build icon- for future impl

--- plugin/src/main/java/consulo/maven/compiler/MavenCompilerRunner.java
+++ plugin/src/main/java/consulo/maven/compiler/MavenCompilerRunner.java
@@ -7,16 +7,19 @@ import consulo.compiler.*;
 import consulo.compiler.scope.CompileScope;
 import consulo.compiler.util.ModuleCompilerUtil;
 import consulo.localize.LocalizeValue;
+import consulo.maven.icon.MavenIconGroup;
 import consulo.maven.module.extension.MavenModuleExtension;
 import consulo.maven.rt.server.common.model.MavenExplicitProfiles;
 import consulo.maven.rt.server.common.model.MavenId;
 import consulo.module.Module;
 import consulo.module.extension.ModuleExtensionHelper;
 import consulo.project.Project;
+import consulo.ui.image.Image;
 import jakarta.annotation.Nonnull;
 import jakarta.inject.Inject;
 import org.jetbrains.idea.maven.execution.MavenRunner;
 import org.jetbrains.idea.maven.execution.MavenRunnerParameters;
+import org.jetbrains.idea.maven.localize.MavenTasksLocalize;
 import org.jetbrains.idea.maven.project.MaveOverrideCompilerPolicy;
 import org.jetbrains.idea.maven.project.MavenGeneralSettings;
 import org.jetbrains.idea.maven.project.MavenProject;
@@ -32,120 +35,111 @@ import java.util.List;
  * @since 18/04/2023
  */
 @ExtensionImpl
-public class MavenCompilerRunner implements CompilerRunner
-{
-	private final Project myProject;
-	private final ModuleExtensionHelper myModuleExtensionHelper;
+public class MavenCompilerRunner implements CompilerRunner {
+    private final Project myProject;
+    private final ModuleExtensionHelper myModuleExtensionHelper;
 
-	@Inject
-	public MavenCompilerRunner(Project project, ModuleExtensionHelper moduleExtensionHelper)
-	{
-		myProject = project;
-		myModuleExtensionHelper = moduleExtensionHelper;
-	}
+    @Inject
+    public MavenCompilerRunner(Project project, ModuleExtensionHelper moduleExtensionHelper) {
+        myProject = project;
+        myModuleExtensionHelper = moduleExtensionHelper;
+    }
 
-	@Nonnull
-	@Override
-	public LocalizeValue getName()
-	{
-		return LocalizeValue.localizeTODO("Maven");
-	}
+    // @Override
+    public Image getBuildIcon() {
+        return MavenIconGroup.mavenlogo();
+    }
 
-	@Override
-	public boolean isAvailable(CompileContextEx compileContextEx)
-	{
-		if(!myModuleExtensionHelper.hasModuleExtension(MavenModuleExtension.class))
-		{
-			return false;
-		}
+    @Nonnull
+    @Override
+    public LocalizeValue getName() {
+        return LocalizeValue.localizeTODO("Maven");
+    }
 
-		MavenGeneralSettings generalSettings = MavenProjectsManager.getInstance(myProject).getGeneralSettings();
-		return generalSettings.getOverrideCompilePolicy() != MaveOverrideCompilerPolicy.DISABLED;
-	}
+    @Override
+    public boolean isAvailable(CompileContextEx compileContextEx) {
+        if (!myModuleExtensionHelper.hasModuleExtension(MavenModuleExtension.class)) {
+            return false;
+        }
 
-	@Override
-	public boolean build(CompileDriver compileDriver, CompileContextEx context, boolean isRebuild, boolean forceCompile, boolean onlyCheckStatus) throws ExitException
-	{
-		MavenGeneralSettings generalSettings = MavenProjectsManager.getInstance(myProject).getGeneralSettings();
-		if(generalSettings.getOverrideCompilePolicy() == MaveOverrideCompilerPolicy.DISABLED)
-		{
-			// must be never entered due #isAvailable() check
-			return false;
-		}
+        MavenGeneralSettings generalSettings = MavenProjectsManager.getInstance(myProject).getGeneralSettings();
+        return generalSettings.getOverrideCompilePolicy() != MaveOverrideCompilerPolicy.DISABLED;
+    }
 
-		MavenProjectsManager mavenProjectsManager = MavenProjectsManager.getInstance(myProject);
-		MavenExplicitProfiles explicitProfiles = mavenProjectsManager.getExplicitProfiles();
+    @Override
+    public boolean build(CompileDriver compileDriver, CompileContextEx context, boolean isRebuild, boolean forceCompile, boolean onlyCheckStatus) throws ExitException {
+        MavenGeneralSettings generalSettings = MavenProjectsManager.getInstance(myProject).getGeneralSettings();
+        if (generalSettings.getOverrideCompilePolicy() == MaveOverrideCompilerPolicy.DISABLED) {
+            // must be never entered due #isAvailable() check
+            return false;
+        }
 
-		CompileScope compileScope = context.getCompileScope();
-		List<String> goals = new ArrayList<>();
-		// do not allow run tests while compilation
-		goals.add("-DskipTests=true");
-		switch(generalSettings.getOverrideCompilePolicy())
-		{
-			case BY_COMPILE:
-				goals.add("compile");
-				if(compileScope.includeTestScope())
-				{
-					goals.add("test-compile");
-				}
-				break;
-			case BY_PACKAGE:
-				goals.add("package");
-				break;
-		}
+        MavenProjectsManager mavenProjectsManager = MavenProjectsManager.getInstance(myProject);
+        MavenExplicitProfiles explicitProfiles = mavenProjectsManager.getExplicitProfiles();
 
-		List<Module> modules = new ArrayList<>(List.of(compileScope.getAffectedModules()));
-		ModuleCompilerUtil.sortModules(context.getProject(), modules);
+        CompileScope compileScope = context.getCompileScope();
+        List<String> goals = new ArrayList<>();
+        // do not allow run tests while compilation
+        goals.add("-DskipTests=true");
+        switch (generalSettings.getOverrideCompilePolicy()) {
+            case BY_COMPILE:
+                goals.add("compile");
+                if (compileScope.includeTestScope()) {
+                    goals.add("test-compile");
+                }
+                break;
+            case BY_PACKAGE:
+                goals.add("package");
+                break;
+        }
 
-		goals.add("-pl");
-		StringBuilder builder = new StringBuilder();
-		int i = 0;
-		for(Module module : modules)
-		{
-			MavenProject mavenProject = mavenProjectsManager.findProject(module);
-			if(mavenProject == null)
-			{
-				continue;
-			}
+        List<Module> modules = new ArrayList<>(List.of(compileScope.getAffectedModules()));
+        ModuleCompilerUtil.sortModules(context.getProject(), modules);
 
-			MavenId mavenId = mavenProject.getMavenId();
+        goals.add("-pl");
+        StringBuilder builder = new StringBuilder();
+        int i = 0;
+        for (Module module : modules) {
+            MavenProject mavenProject = mavenProjectsManager.findProject(module);
+            if (mavenProject == null) {
+                continue;
+            }
 
-			if(i != 0)
-			{
-				builder.append(",");
-			}
+            MavenId mavenId = mavenProject.getMavenId();
 
-			builder.append(mavenId.getGroupId()).append(":").append(mavenId.getArtifactId());
+            if (i != 0) {
+                builder.append(",");
+            }
 
-			i++;
-		}
+            builder.append(mavenId.getGroupId()).append(":").append(mavenId.getArtifactId());
 
-		goals.add(builder.toString());
+            i++;
+        }
 
-		// -am -amd
-		goals.add("-am");
+        goals.add(builder.toString());
 
-		List<MavenProject> rootProjects = mavenProjectsManager.getRootProjects();
-		if(rootProjects.isEmpty())
-		{
-			// sometimes when project was failed to import
-			return false;
-		}
+        // -am -amd
+        goals.add("-am");
 
-		MavenProject mavenProject = rootProjects.get(0);
+        List<MavenProject> rootProjects = mavenProjectsManager.getRootProjects();
+        if (rootProjects.isEmpty()) {
+            // sometimes when project was failed to import
+            return false;
+        }
 
-		MavenRunnerParameters params = new MavenRunnerParameters(true, mavenProject.getDirectory(), goals, explicitProfiles);
-		params.setResolveToWorkspace(true);
+        MavenProject mavenProject = rootProjects.get(0);
 
-		ProgressIndicator indicator = ProgressManager.getGlobalProgressIndicator();
+        MavenRunnerParameters params = new MavenRunnerParameters(true, mavenProject.getDirectory(), goals, explicitProfiles);
+        params.setResolveToWorkspace(true);
 
-		MavenRunner mavenRunner = MavenRunner.getInstance(context.getProject());
+        ProgressIndicator indicator = ProgressManager.getGlobalProgressIndicator();
 
-		if(!mavenRunner.runBatch(Collections.singletonList(params), null, null, TasksBundle.message("maven.tasks.executing"), indicator))
-		{
-			context.addMessage(CompilerMessageCategory.ERROR, "Compilation failed", null, -1, -1);
-		}
+        MavenRunner mavenRunner = MavenRunner.getInstance(context.getProject());
 
-		return true;
-	}
+        if (!mavenRunner.runBatch(Collections.singletonList(params), null, null, MavenTasksLocalize.mavenTasksExecuting().get(), indicator)) {
+            context.addMessage(CompilerMessageCategory.ERROR, "Compilation failed", null, -1, -1);
+        }
+
+        return true;
+    }
 }
