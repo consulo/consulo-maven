72d9cd87e9772d7bd9a27d0c70ed8d133f029ace
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-08-06 16:52:30 +0400

    fixed npe on step back

--- plugin/src/main/java/consulo/maven/importProvider/MavenModuleImportProvider.java
+++ plugin/src/main/java/consulo/maven/importProvider/MavenModuleImportProvider.java
@@ -14,16 +14,16 @@ import consulo.ui.image.Image;
 import consulo.util.lang.StringUtil;
 import consulo.virtualFileSystem.VirtualFile;
 import consulo.virtualFileSystem.util.VirtualFileUtil;
+import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
 import org.jetbrains.idea.maven.MavenIcons;
 import org.jetbrains.idea.maven.importing.MavenDefaultModifiableModelsProvider;
+import org.jetbrains.idea.maven.localize.MavenProjectLocalize;
 import org.jetbrains.idea.maven.project.*;
 import org.jetbrains.idea.maven.utils.MavenUtil;
 import org.jetbrains.idea.maven.wizards.MavenProjectImportStep;
 import org.jetbrains.idea.maven.wizards.SelectImportedProjectsStep;
 import org.jetbrains.idea.maven.wizards.SelectProfilesStep;
-
-import jakarta.annotation.Nonnull;
 
 import java.io.File;
 import java.util.Collection;
@@ -35,153 +35,130 @@ import java.util.function.Consumer;
  * @since 31-Jan-17
  */
 @ExtensionImpl
-public class MavenModuleImportProvider implements ModuleImportProvider<MavenImportModuleContext>
-{
-	@Nonnull
-	@Override
-	public MavenImportModuleContext createContext(@Nullable Project project)
-	{
-		return new MavenImportModuleContext(project);
-	}
+public class MavenModuleImportProvider implements ModuleImportProvider<MavenImportModuleContext> {
+    @Nonnull
+    @Override
+    public MavenImportModuleContext createContext(@Nullable Project project) {
+        return new MavenImportModuleContext(project);
+    }
 
-	@Nonnull
-	@Override
-	public String getName()
-	{
-		return ProjectBundle.message("maven.name");
-	}
+    @Nonnull
+    @Override
+    public String getName() {
+        return MavenProjectLocalize.mavenName().get();
+    }
 
-	@Nonnull
-	@Override
-	public Image getIcon()
-	{
-		return MavenIcons.MavenLogo;
-	}
+    @Nonnull
+    @Override
+    public Image getIcon() {
+        return MavenIcons.MavenLogo;
+    }
 
-	@Nonnull
-	@Override
-	public String getFileSample()
-	{
-		return "<b>Maven</b> project file (pom.xml)";
-	}
+    @Nonnull
+    @Override
+    public String getFileSample() {
+        return "<b>Maven</b> project file (pom.xml)";
+    }
 
-	@Override
-	public boolean canImport(@Nonnull File fileOrDirectory)
-	{
-		if(fileOrDirectory.isDirectory())
-		{
-			return new File(fileOrDirectory, MavenConstants.POM_XML).exists();
-		}
-		else
-		{
-			return MavenConstants.POM_XML.equals(fileOrDirectory.getName());
-		}
-	}
+    @Override
+    public boolean canImport(@Nonnull File fileOrDirectory) {
+        if (fileOrDirectory.isDirectory()) {
+            return new File(fileOrDirectory, MavenConstants.POM_XML).exists();
+        }
+        else {
+            return MavenConstants.POM_XML.equals(fileOrDirectory.getName());
+        }
+    }
 
-	@Override
-	public void buildSteps(@Nonnull Consumer<WizardStep<MavenImportModuleContext>> consumer, @Nonnull MavenImportModuleContext context)
-	{
-		consumer.accept(new MavenProjectImportStep(context));
-		consumer.accept(new SelectProfilesStep(context));
-		consumer.accept(new SelectImportedProjectsStep(context)
-		{
-			@Override
-			protected String getElementText(final MavenProject project)
-			{
-				final StringBuilder stringBuilder = new StringBuilder();
-				stringBuilder.append(project.getMavenId());
-				VirtualFile root = context.getRootDirectory();
-				if(root != null)
-				{
-					final String relPath = VirtualFileUtil.getRelativePath(project.getDirectoryFile(), root, File.separatorChar);
-					if(StringUtil.isNotEmpty(relPath))
-					{
-						stringBuilder.append(" [").append(relPath).append("]");
-					}
-				}
+    @Override
+    public void buildSteps(@Nonnull Consumer<WizardStep<MavenImportModuleContext>> consumer, @Nonnull MavenImportModuleContext context) {
+        consumer.accept(new MavenProjectImportStep(context));
+        consumer.accept(new SelectProfilesStep(context));
+        consumer.accept(new SelectImportedProjectsStep(context) {
+            @Override
+            protected String getElementText(final MavenProject project) {
+                final StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append(project.getMavenId());
+                VirtualFile root = context.getRootDirectory();
+                if (root != null) {
+                    final String relPath = VirtualFileUtil.getRelativePath(project.getDirectoryFile(), root, File.separatorChar);
+                    if (StringUtil.isNotEmpty(relPath)) {
+                        stringBuilder.append(" [").append(relPath).append("]");
+                    }
+                }
 
-				if(!isElementEnabled(project))
-				{
-					stringBuilder.append(" (project is ignored. See Settings -> Maven -> Ignored Files)");
-				}
+                if (!isElementEnabled(project)) {
+                    stringBuilder.append(" (project is ignored. See Settings -> Maven -> Ignored Files)");
+                }
 
-				return stringBuilder.toString();
-			}
+                return stringBuilder.toString();
+            }
 
-			@Override
-			protected boolean isElementEnabled(MavenProject mavenProject)
-			{
-				Project project = myContext.getProject();
-				if(project == null)
-				{
-					return true;
-				}
+            @Override
+            protected boolean isElementEnabled(MavenProject mavenProject) {
+                Project project = myContext.getProject();
+                if (project == null) {
+                    return true;
+                }
 
-				return !MavenProjectsManager.getInstance(project).isIgnored(mavenProject);
-			}
+                return !MavenProjectsManager.getInstance(project).isIgnored(mavenProject);
+            }
 
-			@Override
-			public void updateDataModel()
-			{
-				super.updateDataModel();
-				myContext.setName(context.getSuggestedProjectName());
-			}
-		});
-		consumer.accept(new UnifiedProjectOrModuleNameStep<>(context));
-	}
+            @Override
+            public void updateDataModel() {
+                super.updateDataModel();
+                String suggestedProjectName = context.getSuggestedProjectName();
+                if (suggestedProjectName != null) {
+                    myContext.setName(suggestedProjectName);
+                }
+            }
+        });
+        consumer.accept(new UnifiedProjectOrModuleNameStep<>(context));
+    }
 
-	@RequiredReadAction
-	@Override
-	public void process(@Nonnull MavenImportModuleContext context, @Nonnull Project project, @Nonnull ModifiableModuleModel modifiableModuleModel, @Nonnull Consumer<Module> consumer)
-	{
-		MavenWorkspaceSettings settings = MavenWorkspaceSettingsComponent.getInstance(project).getSettings();
+    @RequiredReadAction
+    @Override
+    public void process(@Nonnull MavenImportModuleContext context, @Nonnull Project project, @Nonnull ModifiableModuleModel modifiableModuleModel, @Nonnull Consumer<Module> consumer) {
+        MavenWorkspaceSettings settings = MavenWorkspaceSettingsComponent.getInstance(project).getSettings();
 
-		settings.generalSettings = context.getGeneralSettings();
-		settings.importingSettings = context.getImportingSettings();
+        settings.generalSettings = context.getGeneralSettings();
+        settings.importingSettings = context.getImportingSettings();
 
-		String settingsFile = System.getProperty("idea.maven.import.settings.file");
-		if(!StringUtil.isEmptyOrSpaces(settingsFile))
-		{
-			settings.generalSettings.setUserSettingsFile(settingsFile.trim());
-		}
+        String settingsFile = System.getProperty("idea.maven.import.settings.file");
+        if (!StringUtil.isEmptyOrSpaces(settingsFile)) {
+            settings.generalSettings.setUserSettingsFile(settingsFile.trim());
+        }
 
-		MavenExplicitProfiles selectedProfiles = context.getSelectedProfiles();
+        MavenExplicitProfiles selectedProfiles = context.getSelectedProfiles();
 
-		String enabledProfilesList = System.getProperty("idea.maven.import.enabled.profiles");
-		String disabledProfilesList = System.getProperty("idea.maven.import.disabled.profiles");
-		if(enabledProfilesList != null || disabledProfilesList != null)
-		{
-			selectedProfiles = selectedProfiles.clone();
-			appendProfilesFromString(selectedProfiles.getEnabledProfiles(), enabledProfilesList);
-			appendProfilesFromString(selectedProfiles.getDisabledProfiles(), disabledProfilesList);
-		}
+        String enabledProfilesList = System.getProperty("idea.maven.import.enabled.profiles");
+        String disabledProfilesList = System.getProperty("idea.maven.import.disabled.profiles");
+        if (enabledProfilesList != null || disabledProfilesList != null) {
+            selectedProfiles = selectedProfiles.clone();
+            appendProfilesFromString(selectedProfiles.getEnabledProfiles(), enabledProfilesList);
+            appendProfilesFromString(selectedProfiles.getDisabledProfiles(), disabledProfilesList);
+        }
 
-		MavenProjectsManager manager = MavenProjectsManager.getInstance(project);
-		manager.addManagedFilesWithProfiles(MavenUtil.collectFiles(context.mySelectedProjects), selectedProfiles);
-		manager.waitForReadingCompletion();
+        MavenProjectsManager manager = MavenProjectsManager.getInstance(project);
+        manager.addManagedFilesWithProfiles(MavenUtil.collectFiles(context.mySelectedProjects), selectedProfiles);
+        manager.waitForReadingCompletion();
 
-		boolean isFromUI = false;
-		List<Module> modules = manager.importProjects(new MavenDefaultModifiableModelsProvider(project));
-		for(Module module : modules)
-		{
-			consumer.accept(module);
-		}
-	}
+        List<Module> modules = manager.importProjects(new MavenDefaultModifiableModelsProvider(project));
+        for (Module module : modules) {
+            consumer.accept(module);
+        }
+    }
 
-	private void appendProfilesFromString(Collection<String> selectedProfiles, String profilesList)
-	{
-		if(profilesList == null)
-		{
-			return;
-		}
+    private void appendProfilesFromString(Collection<String> selectedProfiles, String profilesList) {
+        if (profilesList == null) {
+            return;
+        }
 
-		for(String profile : StringUtil.split(profilesList, ","))
-		{
-			String trimmedProfileName = profile.trim();
-			if(!trimmedProfileName.isEmpty())
-			{
-				selectedProfiles.add(trimmedProfileName);
-			}
-		}
-	}
+        for (String profile : StringUtil.split(profilesList, ",")) {
+            String trimmedProfileName = profile.trim();
+            if (!trimmedProfileName.isEmpty()) {
+                selectedProfiles.add(trimmedProfileName);
+            }
+        }
+    }
 }
