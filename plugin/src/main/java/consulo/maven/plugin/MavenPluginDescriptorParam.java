753e5d30a086ea026d9566f04dfafa1d86d6127b
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-06-17 22:15:26 +0300

    update to last language api

--- plugin/src/main/java/consulo/maven/plugin/MavenPluginDescriptorParam.java
+++ plugin/src/main/java/consulo/maven/plugin/MavenPluginDescriptorParam.java
@@ -1,95 +1,84 @@
 package consulo.maven.plugin;
 
+import consulo.application.Application;
 import consulo.language.Language;
-import consulo.language.LanguagePointerUtil;
+import consulo.language.LanguageRegistry;
 import consulo.maven.internal.plugin.MavenPluginDescriptorParamBuilderImpl;
 import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 import org.jetbrains.idea.maven.plugins.api.MavenParamLanguageProvider;
 import org.jetbrains.idea.maven.plugins.api.MavenParamReferenceProvider;
 import org.jetbrains.idea.maven.plugins.api.MavenSoftAwareReferenceProvider;
 
-import jakarta.annotation.Nullable;
 import java.util.function.Supplier;
 
 /**
  * @author VISTALL
  * @since 20/01/2023
  */
-public final class MavenPluginDescriptorParam
-{
-	private final String myName;
-	private final MavenParamReferenceProvider myRefProvider;
-	@Nonnull
-	private final Supplier<Language> myLanguagePointer;
-	@Nullable
-	private final MavenParamLanguageProvider myLanguageProvider;
-	@Nullable
-	private final String myLanguageInjectionPrefix;
-	@Nullable
-	private final String myLanguageInjectionSuffix;
+public final class MavenPluginDescriptorParam {
+    private final String myName;
+    private final MavenParamReferenceProvider myRefProvider;
+    @Nonnull
+    private final Supplier<Language> myLanguagePointer;
+    @Nullable
+    private final MavenParamLanguageProvider myLanguageProvider;
+    @Nullable
+    private final String myLanguageInjectionPrefix;
+    @Nullable
+    private final String myLanguageInjectionSuffix;
 
-	public MavenPluginDescriptorParam(String name, MavenPluginDescriptorParamBuilderImpl builder)
-	{
-		myName = name;
-		myRefProvider = builder.myRefProvider.get();
-		if(builder.myLanguageId != null)
-		{
-			myLanguagePointer = LanguagePointerUtil.createPointer(builder.myLanguageId);
-		}
-		else
-		{
-			myLanguagePointer = () -> null;
-		}
+    public MavenPluginDescriptorParam(String name, MavenPluginDescriptorParamBuilderImpl builder) {
+        myName = name;
+        myRefProvider = builder.myRefProvider.get();
+        if (builder.myLanguageId != null) {
+            myLanguagePointer = Application.get().getInstance(LanguageRegistry.class).createLanguagePointer(builder.myLanguageId);
+        }
+        else {
+            myLanguagePointer = () -> null;
+        }
 
-		myLanguageProvider = builder.myLanguageProvider.get();
+        myLanguageProvider = builder.myLanguageProvider.get();
 
-		myLanguageInjectionPrefix = builder.myLanguageInjectionPrefix;
+        myLanguageInjectionPrefix = builder.myLanguageInjectionPrefix;
 
-		myLanguageInjectionSuffix = builder.myLanguageInjectionSuffix;
+        myLanguageInjectionSuffix = builder.myLanguageInjectionSuffix;
 
-		if(builder.mySoft != null)
-		{
-			if(myRefProvider == null)
-			{
-				throw new IllegalArgumentException("#soft can't not be without refProvider");
-			}
+        if (builder.mySoft != null) {
+            if (myRefProvider == null) {
+                throw new IllegalArgumentException("#soft can't not be without refProvider");
+            }
 
-			((MavenSoftAwareReferenceProvider) myRefProvider).setSoft(builder.mySoft);
-		}
-	}
+            ((MavenSoftAwareReferenceProvider) myRefProvider).setSoft(builder.mySoft);
+        }
+    }
 
-	@Nullable
-	public Language getLanguage()
-	{
-		return myLanguagePointer.get();
-	}
+    @Nullable
+    public Language getLanguage() {
+        return myLanguagePointer.get();
+    }
 
-	@Nullable
-	public MavenParamLanguageProvider getLanguageProvider()
-	{
-		return myLanguageProvider;
-	}
+    @Nullable
+    public MavenParamLanguageProvider getLanguageProvider() {
+        return myLanguageProvider;
+    }
 
-	public String getName()
-	{
-		return myName;
-	}
+    public String getName() {
+        return myName;
+    }
 
-	@Nullable
-	public MavenParamReferenceProvider getRefProvider()
-	{
-		return myRefProvider;
-	}
+    @Nullable
+    public MavenParamReferenceProvider getRefProvider() {
+        return myRefProvider;
+    }
 
-	@Nullable
-	public String getLanguageInjectionPrefix()
-	{
-		return myLanguageInjectionPrefix;
-	}
+    @Nullable
+    public String getLanguageInjectionPrefix() {
+        return myLanguageInjectionPrefix;
+    }
 
-	@Nullable
-	public String getLanguageInjectionSuffix()
-	{
-		return myLanguageInjectionSuffix;
-	}
+    @Nullable
+    public String getLanguageInjectionSuffix() {
+        return myLanguageInjectionSuffix;
+    }
 }
